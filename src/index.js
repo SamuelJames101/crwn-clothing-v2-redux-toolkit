bee5324508997d403e2feef7ad3ef59419c751ea
SamuelJames101 <SamuelJames101101@gmail.com>
2023-09-15 16:14:58 +0100

    stripe work

--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
+import { stripePromise, pk } from './utils/stripe/stripe.utils';
 import App from './App';
 import { store, persistor } from './store/store';
 
 import './index.scss';
 
 const rootElement = document.getElementById('root');
-
+console.log("pk key " + pk);
 render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <PersistGate loading={null} persistor={persistor}> 
         <BrowserRouter>
+        <Elements stripe={stripePromise}>
           <App />
+        </Elements>     
         </BrowserRouter>
-      {/* </PersistGate> */}
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
